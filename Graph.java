56c4b118b9b7803b1c82ddc5cee33f4f7e7c6d4e
josh <joskar201@gmail.com>
2017-07-27 10:54:02 +0300

    created the graph data type api.

---
+++ Graph.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+    private final int V;
+    private ArrayList<Integer>[] adj;//vertex indexed array of adjacency lists
+
+    public Graph(int v) {
+        this.V = v;
+        adj = (ArrayList<Integer>[]) new ArrayList[v];
+        for (int i=0; i < v; i++)
+            adj[i] = new ArrayList<>();
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public List<Integer> adj(int v) {
+        return adj[v];
+    }
+    public int V(){
+        return this.V;
+    }
+    //find a faster way to do this
+    public int E(){
+        int count = 0;
+        for (int v=0;v<this.V;v++)
+            for (int w:this.adj(v))
+                count++;
+        return count/2;
+    }
+}
